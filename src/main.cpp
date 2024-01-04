e300df51ace6e35c854650d55f2b8265ace972b4
sonoguerra <151522030+sonoguerra@users.noreply.github.com>
2024-01-04 21:19:56 +0100

    first commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,20 @@
-#include <vector>
-#include "../src/humanPlayer.cpp"
+//Guerra Thomas
+
 #include <iostream>
-#include "../src/exchangeMoney.cpp"
-
-
-int main(){	
+#include <cstring>
+#include "../include/game.h"
+int main(int argc, char* argv[]) {
+	/*
+	 * Controlli argomento passato da riga di comando
+	 */
+	if(argc<2) {
+		std::cout << "Non hai fornito il parametro da riga di comando" << std::endl;
+		return -1;
+	}
 	
+	if(strcmp(argv[1], "computer")!=0 && strcmp(argv[1], "human")!=0) {
+		std::cout << "Parametro non valido, inserire: \"computer\" o \"human\"" << std::endl;
+		return -1;
+	}
 	return 0;
-
 }
