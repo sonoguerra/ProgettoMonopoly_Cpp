88f31b71681076f56d420323d766632a0c9536b6
DavidoOwO <davidepinzan@libero.it>
2024-01-06 21:53:21 +0100

    Modifiche alla classe Square per aggiungere metodi di utilità'

--- include/square.h
+++ include/square.h
@@ -8,17 +8,23 @@
 class Square {
     public:
         Square();
-        Square(std::string category); //da vedere se usare un enum di char, ma è una feature di C++11. Vedere se si può quantomeno convertire a char.
+        Square(std::string position, std::string category); //da vedere se usare un enum di char, ma è una feature di C++11. Vedere se si può quantomeno convertire a char.
 // aggiungere costruttore di copia e spostamento(?)
         std::string get_content() const;
+        bool is_unassigned() const;
         bool is_house_built() const;
         bool is_hotel_built() const;
+        char get_type() const;
+        int get_owner() const;
         void add_player(int player);
         void remove_player(int player);
+        void buy_property(int player);
         void upgrade();
         void delete_property();
     private:
+        std::string id;
         std::string content;
+        int owner;
 };
 
 std::ostream& operator<<(std::ostream& o, const Square& s);
--- src/square.cpp
+++ src/square.cpp
@@ -3,17 +3,25 @@
 Square::Square() {
 
     content = "| |";
+    owner = 0;
 
 }
 
-Square::Square(std::string category) {
+Square::Square(std::string position, std::string category) {
     if (category != " " && category != "E" && category != "S" && category != "L" && category != "P") throw std::invalid_argument("Invalid argument: only valid arguments are characters P, S, L, E and blank space.");
     content = "|" + category + "|";
+    id = position;
+    owner = 0;
 }
-
 
 std::string Square::get_content() const {
     return content;
+}
+
+bool Square::is_unassigned() const {
+
+    if (owner == 0) return true;
+    return false;
 }
 
 bool Square::is_house_built() const {
@@ -23,6 +31,18 @@ bool Square::is_house_built() const {
 bool Square::is_hotel_built() const {
     return (content.at(2) == '^');
 }
+
+char Square::get_type() const {
+    return content.at(1);
+}
+
+int Square::get_owner() const {
+    return owner;
+}
+
+
+
+
 
 void Square::add_player(int player) {
     content.insert(content.length()-1, std::to_string(player));
@@ -38,17 +58,13 @@ void Square::upgrade() {
     else if (!is_hotel_built()) content.insert(2, "*");
 }
 
-
-
-
-
-
-
-
-
+void Square::buy_property(int player) {
+    owner = player;
+}
 
 void Square::delete_property() {
     if (is_hotel_built() || is_house_built()) content.erase(2, 1);
+    owner = 0;
 }
 
 std::ostream& operator<<(std::ostream& o, const Square& s) {
