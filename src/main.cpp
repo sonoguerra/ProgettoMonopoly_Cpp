fe455adfb4eafc824facd7c994181fb9471a7b5e
sonoguerra <151522030+sonoguerra@users.noreply.github.com>
2024-01-07 10:55:23 +0100

    tabellone update

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,15 @@
-//Guerra Thomas
-
 #include <iostream>
-#include <cstring>
-#include "../include/game.h"
+#include <string>
+#include "../include/board.h"
 
-int main(int argc, char* argv[]) {
-	/*
-	 * Controlli argomento passato da riga di comando
-	 */
-	if(argc<2) {
-		std::cout << "Non hai fornito il parametro da riga di comando" << std::endl;
-		return -1;
-	}
-	
-	if(strcmp(argv[1], "computer")!=0 && strcmp(argv[1], "human")!=0) {
-		std::cout << "Parametro non valido, inserire: \"computer\" o \"human\"" << std::endl;
-		return -1;
-	}
-	return 0;
+int main() {
+    Board b;
+    b.move(1, 8);
+    b.move(1, 8);
+    std::cout << b << std::endl;
+    b.upgrade_terrain(10);
+    std::cout << b << std::endl;
+    b.delete_property(10);
+    std::cout << b;
+    return 0;
 }
